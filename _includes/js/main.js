8d57eec3f6cc26c5e548ca200d9d0a6dfc89bd3b
Devuiux <dev.uiux@yahoo.com>
2016-09-18 17:47:29 +0300

    added fixed card

--- _includes/js/main.js
+++ _includes/js/main.js
@@ -1,18 +1,22 @@
-
-
-$(document).ready(function(){
+$(document).ready(function() {
 
     Array.prototype.forEach.call(document.querySelectorAll('.mdl-card__media'), function(el) {
-      var link = el.querySelector('a');
-      if(!link) {
-        return;
-      }
-      var target = link.getAttribute('href');
-      if(!target) {
-        return;
-      }
-      el.addEventListener('click', function() {
-        location.href = target;
-      });
+        var link = el.querySelector('a');
+        if (!link) {
+            return;
+        }
+        var target = link.getAttribute('href');
+        if (!target) {
+            return;
+        }
+        el.addEventListener('click', function() {
+            location.href = target;
+        });
     });
+
+    $('.right-wrapper').outerWidth($('.col-right').width());
+
+    $(window).resize(function(){
+	    $('.right-wrapper').outerWidth($('.col-right').width());
+	});
 });
